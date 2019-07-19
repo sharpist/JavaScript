1ed934e13b913f16258d8a76b5cf1526db479b41
Alexander Usov <webapp@programmer.net>
2019-07-20 01:52:15 +0300

    Promise

---
+++ Promise/Promise.js
@@ -0,0 +1,22 @@
+﻿function sum(a, b)
+{
+    return new Promise(function(resolve, reject)
+    {
+        setTimeout(function()
+        {
+            if (typeof a !== "number" || typeof b !== "number")
+                return reject(new TypeError("ввод должен быть числом"));
+
+            return resolve(a + b);
+        }, 1000); // отправить ответ через 1 сек
+    });
+}
+
+var myPromise = sum(10, 5) // 10 + 5 = 15
+myPromise.then(function(res) {
+    $('#output').append('<p>10 + 5 = '+res+'</p>'); // div id="output"
+})
+.catch(function(err) {
+    document.write(err.message);
+    console.log(err.message);
+});

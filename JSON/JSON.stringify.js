14300aa9730ffd0abc29d0c99e6f2eccaf3ffd2b
Alexander Usov <webapp@programmer.net>
2019-07-20 04:56:17 +0300

    JSON.stringify

--- JSON/JSON.stringify.js
+++ JSON/JSON.stringify.js
@@ -1,10 +1,16 @@
 ﻿var event = {
     title: "Конференция",
-    date: "2019-10-20T12:00:00.000Z"
+    date: "2019-10-20T12:00:00.000Z",
+    window: window // преобразовать невозможно!
 };
 // преобразование значения в JSON-строку
-var str = JSON.stringify(event);
-
+var str = JSON.stringify(event, ["title", "date"]); // массив свойств, подлежащих сериализации
+/*
+var str = JSON.stringify(event, function(key, value) { // свойство с названием window игнорируется
+    return (key == 'window') ?
+        undefined : value;
+});
+*/
 // интеллектуальное восстановление из строки
 var event = JSON.parse(str, function(key, value) {
     return (key == 'date') ?
